093b83b34fb8ae1ccec46e54bdf7e2f90d807137
moe <moe.duffdude@gmail.com>
2014-06-15 17:49:11 +0300

    prediction + stat

--- PIFA/assets/js/app.js
+++ PIFA/assets/js/app.js
@@ -2,10 +2,35 @@ var app = angular.module('PIFA', []);
 
 app.controller('PIFACtrl', ['$scope', function ($scope) {
   $scope.players = {
-    eyassu: 0,
-    joey: 0,
-    moe: 0
-  }
+    eyassu: {
+      spotOn: 0,
+      right: 0,
+      wrong: 0,
+      forfeit: 0,
+      PTS: 0
+    },
+    joey: {
+      spotOn: 0,
+      right: 0,
+      wrong: 0,
+      forfeit: 0,
+      PTS: 0
+    },
+    kaleb: {
+      spotOn: 0,
+      right: 0,
+      wrong: 0,
+      forfeit: 0,
+      PTS: 0
+    },
+    moe: {
+      spotOn: 0,
+      right: 0,
+      wrong: 0,
+      forfeit: 0,
+      PTS: 0
+    }
+  };
 
   $scope.matches = [
     {
@@ -110,27 +135,87 @@ app.controller('PIFACtrl', ['$scope', function ($scope) {
         $scope.matches[match].prediction[player] = {};
         $scope.matches[match].prediction[player].teamA = 'X';
         $scope.matches[match].prediction[player].teamB = 'X';
-        $scope.players[player] -= 2;
+        $scope.players[player].PTS -= 2;
+        $scope.players[player].forfeit++;
         $scope.matches[match].pts[player] = -2;
       }
 
       // spot on prediction
       else if (($scope.matches[match].match.scoreA === $scope.matches[match].prediction[player].teamA) && ($scope.matches[match].match.scoreB === $scope.matches[match].prediction[player].teamB)) {
-        $scope.players[player] += 3;
+        $scope.players[player].PTS += 3;
+        $scope.players[player].spotOn++;
         $scope.matches[match].pts[player] = 3;
       }
 
       // side prediction
       else if (($scope.matches[match].match.scoreA > $scope.matches[match].match.scoreB) === ($scope.matches[match].prediction[player].teamA > $scope.matches[match].prediction[player].teamB)) {
-        $scope.players[player] += 1;
+        $scope.players[player].PTS += 1;
+        $scope.players[player].right++;
         $scope.matches[match].pts[player] = 1;
       }
 
       // wrong prediction
       else {
-        $scope.players[player] -= 1;
+        $scope.players[player].PTS -= 1;
+        $scope.players[player].wrong++;
         $scope.matches[match].pts[player] = -1;
       }
     }
   }
+
+  // calculating ranking...
+  // it'll be `easier` to do it this way - I've don't deserve to use Angular
+  // forgiveness, please
+  $scope.playersList = [];
+  for (player in $scope.players) {
+    $scope.playersList.push({name: player, stat: $scope.players[player]});
+  };
+
+  $scope.nextPredictions = [
+    {
+      match: {
+        teamA: 'SWI',
+        scoreA: 0,
+        teamB: 'ECU',
+        scoreB: 0
+      },
+      prediction: {
+        joey: {teamA: 1, teamB: 3},
+        moe: {teamA: 0, teamB: 2},
+        eyassu: {teamA: 1, teamB: 1},
+        kaleb: {teamA: 2, teamB: 0},
+      }
+    },
+
+    {
+      match: {
+        teamA: 'FRA',
+        scoreA: 0,
+        teamB: 'HON',
+        scoreB: 0
+      },
+      prediction: {
+        joey: {teamA: 1, teamB: 0},
+        moe: {teamA: 3, teamB: 0},
+        eyassu: {teamA: 2, teamB: 1},
+        kaleb: {teamA: 2, teamB: 1}
+      }
+    },
+
+    {
+      match: {
+        teamA: 'ARG',
+        scoreA: 0,
+        teamB: 'BOS',
+        scoreB: 0
+      },
+      prediction: {
+        joey: {teamA: 4, teamB: 2},
+        moe: {teamA: 4, teamB: 1},
+        eyassu: {teamA: 3, teamB: 1},
+        kaleb: {teamA: 2, teamB: 0}
+      }
+    }
+  ];
+
 }]);
